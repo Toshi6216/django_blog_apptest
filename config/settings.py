41da0fc02c21d5e9a7f7917a3605aec1cee54400
toshi <d-toshi@i.softbank.jp>
2022-10-31 13:44:06 +0900

    deploy

--- config/settings.py
+++ config/settings.py
@@ -85,17 +85,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-import dj_database_url
-from dotenv import (
-    find_dotenv,
-    load_dotenv,
-)
-load_dotenv(find_dotenv())
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+#import dj_database_url
+#from dotenv import (
+#    find_dotenv,
+#    load_dotenv,
+#)
+#load_dotenv(find_dotenv())
+#DATABASES = {
+#    'default': dj_database_url.config(conn_max_age=600),
+#}
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
