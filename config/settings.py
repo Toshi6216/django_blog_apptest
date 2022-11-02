7f457495d3a27a3c93f125c262e20f4fc62c6c21
toshi <d-toshi@i.softbank.jp>
2022-11-02 16:15:17 +0900

    heroku設定

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .settings_local import *
+#from .settings_local import *
+SECRET_KEY = 'django-insecure-zke&_9ffs=41a5++l@3j-btowvwz@9a(i1itgb$v=hjp$9oe%-'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
